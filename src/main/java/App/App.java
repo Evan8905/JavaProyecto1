dd115a37b8ef0b0a44bd3f2c9ee24c15583e387c
Evan Duarte <e.efrenduarte@gmail.com>
2023-11-25 21:36:43 -0600

    Proyecto java

---
+++ src/main/java/App/App.java
@@ -0,0 +1,17 @@
+
+package App;
+
+/**
+ *
+ * @author eefre
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
